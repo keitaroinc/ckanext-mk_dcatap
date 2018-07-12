0b2b040ddbd6276e08a90294f195531c6fc9f622
Pavle Jonoski <pavle.jonoski@keitaro.com>
2018-07-12 16:29:08 +0200

    Added special validators for request_for_data datasets.

--- ckanext/mk_dcatap/plugin.py
+++ ckanext/mk_dcatap/plugin.py
@@ -1,9 +1,12 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+from ckanext.mk_dcatap.validators import request_data_only_members_in_org_validator, request_data_only_required
+
 
 class Mk_DcatapPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IValidators)
 
     # IConfigurer
 
@@ -11,3 +14,9 @@ class Mk_DcatapPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'mk_dcatap')
+    
+    def get_validators(self):
+        return {
+            'request_data_only_members_in_owner_org': request_data_only_members_in_org_validator,
+            'request_data_only_required': request_data_only_required
+        }
---
+++ ckanext/mk_dcatap/validators.py
@@ -0,0 +1,73 @@
+from ckan.plugins.toolkit import _, get_action, request, get_validator
+from ckan.logic import NotFound
+
+
+not_empty = get_validator('not_empty')
+
+def request_data_only_members_in_org_validator(key, data, errors, context):
+    print "   =====> params: ", request.params.get("metadata")
+    if not request.params.get("metadata"):
+        return
+    return members_in_org_validator(key, data, errors, context)
+
+
+def request_data_only_required(key, data, errors, context):
+    print "   =====> params: ", request.params.get("metadata")
+    if not request.params.get("metadata"):
+        return
+    if not data.get(key):
+        errors[key].append(_('Required field'))
+
+
+def members_in_org_validator(key, data, errors, context):
+    maintainers = data[key].split(',')
+    model = context['model']
+    owner_org = data.get(('owner_org',))
+    if owner_org is None:
+        owner_org = data.get('owner_org')
+    print ' ===> owner org ->', owner_org
+    print ' ===> type:', type(owner_org)
+    data_dict = {
+        'id': owner_org
+    }
+    user_ids = []
+    try:
+        members_in_org = get_action('member_list')(context, data_dict)
+    except NotFound:
+        members_in_org = []
+
+    # member_list returns more than just users, so we need to extract only
+    # users
+    
+    members_in_org = [member for member in members_in_org
+                      if member[1] == 'user']
+
+    for email in maintainers:
+        user = model.User.by_email(email)
+
+        if not user:
+            user = model.User.get(email)
+
+        user_found = False
+
+        if user:
+            if type(user) == list:
+                user = user[0]
+
+            for member in members_in_org:
+                if member[0] == user.id:
+                    user_found = True
+
+            if user_found:
+                user_ids.append(user.id)
+            else:
+                message = _('The user with email "{0}" is not part of this '
+                            'organization.').format(email)
+                errors[key].append(message)
+
+        else:
+            message = _('The user with email "{0}" does not exist.'
+                        ).format(email)
+            errors[key].append(message)
+
+    data[key] = ','.join(user_ids)
